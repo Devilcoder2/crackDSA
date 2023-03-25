4038711d467e8a9844f3ce637e461bdb83bd0e0f
Devilcoder2 <113927776+Devilcoder2@users.noreply.github.com>
2023-03-25 10:23:39 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './components/Card';
+import OuterCard from './components/OuterCard';
+
+const dummyData = [
+  {companyName: 'Company1', duration: '2 months', location: 'online', lastDateToApply: '25th'},
+  {companyName: 'Company2', duration: '3 months', location: 'online', lastDateToApply: '26th'},
+  {companyName: 'Company3', duration: '4 months', location: 'online', lastDateToApply: '24th'},
+]
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        dummyData.map((company)=><Card name={company.companyName} duration={company.duration} location={company.location} lastDateToApply={company.lastDateToApply} />)
+      }
     </div>
   );
 }
